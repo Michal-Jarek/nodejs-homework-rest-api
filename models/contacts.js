267ab437b84fd9191b440b848794f52751063840
Michał Jarek <miciu77@gmail.com>
2023-02-11 11:55:19 +0100

    Refactored to missed contacts.js usage

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,40 @@
-// const fs = require('fs/promises')
+import fs from "fs/promises";
+import path from "path";
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("./models/contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await fs
+    .readFile(contactsPath, { encoding: "utf-8" })
+    .then((data) => JSON.parse(data))
+    .catch((err) => console.log(err.message));
+  return contacts;
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contactArray = await listContacts();
+    const contactById = contactArray.find(
+      (data) => data.id === contactId.toString()
+    );
+    if (!contactById) throw new Error({status: "Not found",
+       code: 404,});
+    else return contactById;
+  } catch (err) {
+    return err;
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {};
 
-module.exports = {
+const updateContact = async (contactId, body) => {};
+
+export {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,46 @@
 import express from "express";
+import { addContact, getContactById, listContacts, removeContact } from "../../models/contacts.js";
+
 
 const contactsRouter = express.Router();
 
-contactsRouter.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
 
-contactsRouter.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.get("/", async (req, res, next) => {
+  res.json({
+    status: "succes",
+    code: 200,
+    data: await listContacts(),
+  });
+});
 
-contactsRouter.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.get("/:contactId", async (req, res, next) => {
+  const id = req.params.contactId;
+  const contact = await getContactById(id);
+  console.log(contact.code);
+  res.status(404);
+  // if (contact)
+  //   res.status(200).json({
+  //     status: "succes",
+  //     code: 200,
+  //     data: contact,
+  //   });
+  // else
+  //   res.status(404).json({
+  //     status: "Not found",
+  //     code: 404,
+  //   });
+});
 
-contactsRouter.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-contactsRouter.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+contactsRouter.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+contactsRouter.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
 export default contactsRouter;
